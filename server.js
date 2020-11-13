f9e620b3907a28b92134ee9a406cd772720a81b5
Jay Lewis <jr_lewis@outlook.com>
2020-11-13 15:48:43 +0000

    Updated to server for deployment

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json({ extended: false }));
 
-app.get("/" , (req,res) => res.send("API running"));
+// app.get("/" , (req,res) => res.send("API running"));
 
 app.use(
   cors({
